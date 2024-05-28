6df02661212a73aa4287306f365810477e84655f
Denpas404 <102860689+Denpas404@users.noreply.github.com>
2024-05-28 16:21:28 +0200

    updated the Rtos

--- 03Tirsdag/RtosTirsdag/src/main.cpp
+++ 03Tirsdag/RtosTirsdag/src/main.cpp
@@ -1,50 +1,109 @@
 #include <Arduino.h>
 
-//Defind the LED pin and switch pin
-#define LED_PIN 2
-#define SWITCH_PIN 3
 
-//Define 3 taskhandle
-TaskHandle_t Task1Handler;
-TaskHandle_t Task2Handler;
-TaskHandle_t Task3Handler;
+// Define the LED pin
+const int ledPin = 2;
+const int switchPin = 27; // Switch pin
 
+// Define semaphore
+SemaphoreHandle_t _semaphore;
 
-//Define 3 task functions
-void Task1(void *pvParameters);
-void Task2(void *pvParameters);
-void Task3(void *pvParameters);
+// Define task handles
+TaskHandle_t task1Handle;
+TaskHandle_t task2Handle;
+TaskHandle_t task3Handle;
 
-//Define a semaphore
-SemaphoreHandle_t xSemaphore;
+// Define task
+void task1(void *pvParameters);
+void task2(void *pvParameters);
+void task3(void *pvParameters);
 
-//Define a token
-bool token = false;
+// Define Token
+bool task1Token;
+
 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  // Declare the semaphore
+  _semaphore = xSemaphoreCreateBinary();
+  xSemaphoreGive(_semaphore);
+
+  // Declare the taskhandles
+  task1Handle = NULL;
+  task2Handle = NULL;
+  task3Handle = NULL;
+
+  // Declare the token
+  task1Token = false;
+
+  // Declare tasks
+  xTaskCreate(task1, "Task 1", 2048, NULL, 1, &task1Handle);
+  xTaskCreate(task2, "Task 2", 2048, NULL, 1, &task2Handle);
+  xTaskCreate(task3, "Task 3", 2048, NULL, 999, &task3Handle);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly: 
+  // Loop should never be used in FreeRTOS
 }
 
-//Declare task1
-void Task1(void *pvParameters)
-{
-  
+
+
+// Task 1 
+void task1(void *pvParameters) {
+  while (1) {
+    if (task1Token) {
+      if (xSemaphoreTake(_semaphore, portMAX_DELAY) == pdTRUE) {
+        Serial.println("Task 1 blinking LED");
+
+        digitalWrite(ledPin, HIGH);
+        vTaskDelay(3000 / portTICK_PERIOD_MS);
+        digitalWrite(ledPin, LOW);
+        vTaskDelay(7000 / portTICK_PERIOD_MS); 
+        xSemaphoreGive(_semaphore);
+
+      }
+    } else {
+      vTaskDelay(100 / portTICK_PERIOD_MS); 
+    }
+    vTaskPrioritySet(task2Handle, 2);
+    vTaskPrioritySet(task1Handle, 1);
+  }
 }
 
-//Declare task2
-void Task2(void *pvParameters)
-{
-  
+// Task 2 
+void task2(void *pvParameters) {
+  while (1) {
+    if (xSemaphoreTake(_semaphore, portMAX_DELAY) == pdTRUE) {
+      Serial.println("Task 2 blinking LED");
+      for (int i = 0; i < 20; i++) {
+        digitalWrite(ledPin, HIGH);
+        vTaskDelay(500 / portTICK_PERIOD_MS); 
+        digitalWrite(ledPin, LOW);
+        vTaskDelay(500 / portTICK_PERIOD_MS); 
+      }
+      xSemaphoreGive(_semaphore);
+
+
+    }
+    vTaskPrioritySet(task1Handle, 2); 
+    vTaskPrioritySet(task2Handle, 1); 
+  }
 }
 
-//Declare task3
-void Task3(void *pvParameters)
-{
-  
+
+// Task 3 
+void task3(void *pvParameters) {
+  pinMode(switchPin, INPUT_PULLUP);
+  while (1) {
+    if (digitalRead(switchPin) == LOW) { 
+    task1Token = !task1Token; 
+    Serial.print("Task state: " );
+    Serial.println(String(task1Token));
+    }
+    vTaskDelay(100 / portTICK_PERIOD_MS); 
+  }
 }
